b06873db66d080fe877295ea09bb9acd441aee0c
veera sekhar <68839323+sekhar183@users.noreply.github.com>
2021-03-30 18:27:00 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide")#sets page wide
 st.markdown("""<style>.big-head {font-size:45px !important;text-align:center;text-decoration: underline;font-family: TimesNewRoman;color:white}</style>""", unsafe_allow_html=True)
 st.markdown('<p class="big-head">PR SENSOR DATA</p>', unsafe_allow_html=True)
 #note
-st.write("<i>*Note: All figures are filtered by filters except for distribution plot*<i>")
+st.write("*Note: All figures are filtered by filters except for distribution plot*")
 #Loading data
 zf = zipfile.ZipFile('temphum_all.zip') 
 dataframe = pd.read_csv(zf.open('temphum_all.csv'))
