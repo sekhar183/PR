8b0a96ac18f64a664176e7e77cc68b97069e54ba
veera sekhar <68839323+sekhar183@users.noreply.github.com>
2021-03-30 18:07:52 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,77 +1,120 @@
-#impoerting necessary libraries
-import pandas as pd
+import streamlit as st 
 import numpy as np 
-import datetime as dt
-from datetime import datetime,timedelta
-from tqdm import tqdm
-from requests import get
-from datetime import date
-import warnings
+import pandas as pd 
+import datetime
+from datetime import date, timedelta
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go 
+from plotly.subplots import make_subplots
 import zipfile
-warnings.filterwarnings('ignore') #to ignore warnings
+st.set_page_config(layout="wide")#sets page wide
 
-#history_file
-zf = zipfile.ZipFile('temphum_all.zip')
-df=pd.read_csv(zf.open('temphum_all.csv'))
+#Loading data
+zf = zipfile.ZipFile('temphum_all.zip') 
+dataframe = pd.read_csv(zf.open('temphum_all.csv'))
+dataframe['humid']=dataframe['humid'].replace(99,np.nan)
 
-#past data is already collected(cleaned) and stored in csv file.
-#new data is collected and cleaned and merged with past file.
+#Filtering data using filters(only considering 'time' for now)
+def filt(dataframe,start_date,end_date):
+	df=dataframe[(dataframe['time']<=str(end_date+timedelta(1)))&(dataframe['time']>=str(start_date))]
+	return df
 
-#to get last date in history file
-df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
-last_sample=df.sort_values(by=['time'],ascending=False).reset_index(drop=True).iloc[0]
-last_sample_date=last_sample['time'].strftime('%Y-%m-%d')
-date_list=pd.date_range(last_sample_date,datetime.today()) #creates a list of dates where data needs to be extracted
+#Filters
 
-#authorization and required rooms
-req_temp_ids=[149,150,151,152,153,154,155]
-url="https://water-iq.herokuapp.com/api/iq/sensors/"
-Token='Token token=AC6b88d2d5fc648a1527cedb4b6c21f0'
-auth = {'Authorization': Token}
+start_date = st.sidebar.date_input('start date', date.today()-timedelta(5),min_value=datetime.datetime(2020,11,13),max_value=date.today())#start date
+end_date = st.sidebar.date_input('end date', date.today(),max_value=date.today())#end date
+ft=filt(dataframe,start_date,end_date)
 
-#initiating Empty dataframe
-df_api=pd.DataFrame(columns=['sensor_id', 'sensor_type', 'prev_page', 'current_page', 'next_page',
-       'id', 'amb_temp', 'obj_temp', 'batt', 'time', 'count1', 'count2',
-       'humid', 'status', 'triggered', 'tslice'])
+#filtering data using filters
+st.markdown("""<style>.big-font2 {font-size:30px !important;text-align:center;text-decoration: underline;font-family: fantasy;background-color:#31333F;color:white}</style>""", unsafe_allow_html=True)
+st.markdown('<p class="big-font2">Filtered DataFrame</p>', unsafe_allow_html=True)
+st.dataframe(ft)#showing data frame in App
+st.write(ft.shape)
+with st.beta_expander("code"):
+     st.code('''def filt(dataframe,start_date,end_date):
+     	df=dataframe[(dataframe["time"]<=str(end_date+timedelta(1)))&(dataframe["time"]>=str(start_date))]
+     	return df''', language="python")
 
-#extracting data from api
-for ids in tqdm(req_temp_ids,position=0):
-    for dates in date_list:
-        dat = get('https://water-iq.herokuapp.com/api/iq/sensors/'+str(ids)+'/sensor_logs?search[created_at_lteq]='+str(dates),headers=auth)
-        df2 =pd.DataFrame(dat.json())
-        df1=pd.json_normalize(df2['logs'])
-        df3=pd.concat([df2,df1],axis=1).drop('logs',1)
-        df_api=df_api.append(df3)
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Seperating data
+#OFC
+ft_ofc=ft[ft['location']=='PR Home - OFC']
+#MBW
+ft_mbw=ft[ft['location']=='PR Home - MBW']
+#KIT
+ft_kit=ft[ft['location']=='PR Home - KIT']
+#LVR
+ft_lvr=ft[ft['location']=='PR Home - LVR']
+#MWR
+ft_mwr=ft[ft['location']=='PR Home - MWR']
+#LRM
+ft_lrm=ft[ft['location']=='PR Home - LRM']
+#MBE
+ft_mbe=ft[ft['location']=='PR Home - MBE']
 
-# mapping room id with room name
-room_dict={149: 'PR Home - OFC',150: 'PR Home - MBE',151: 'PR Home - MBW',152: 'PR Home - MWR',153: 'PR Home - LRM',154: 'PR Home - KIT',155: 'PR Home - LVR'}
-df_api['location']=df_api['sensor_id'].map(room_dict)
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#our required locations
-req_loc=['PR Home - MBW','PR Home - KIT','PR Home - LVR','PR Home - MWR','PR Home - OFC','PR Home - LRM','PR Home - MBE']
-df_api=df_api[df_api['location'].isin(req_loc)]
+st.markdown("""<style>.big-font {font-size:30px !important;text-align:center;text-decoration: underline;font-family: fantasy;background-color:#31333F;color:white}</style>""", unsafe_allow_html=True)
+st.markdown('<p class="big-font">STATS</p>', unsafe_allow_html=True)
 
-# cleaning
-df_api['time']=df_api['time'].apply(lambda x:x.replace('T'," "))
-df_api['time']=df_api['time'].apply(lambda x:x.replace('Z',""))
+result_df=ft[['location','obj_temp','humid']].groupby(['location']).describe()
+result_cols=result_df.columns
+#summary_dict={'PR Home - OFC':ft_ofc.shape[0],'PR Home - MBE':ft_mbe.shape[0],'PR Home - MBW':ft_mbw.shape[0],'PR Home - MWR':ft_mwr.shape[0],'PR Home - LRM':ft_lrm.shape[0],'PR Home - KIT':ft_kit.shape[0],'PR Home - LVR':ft_lvr.shape[0]}
+#result_df['no.of samples']=result_df.index.map(summary_dict)
+#result_cols.insert(0,('samples', 'count'))
+st.dataframe(result_df.style.highlight_max(axis=0,color='grey'))
+with st.beta_expander("code"):
+     st.code('''result_df=ft[['location','obj_temp','humid']].groupby(['location']).describe()
+st.dataframe(result_df.style.highlight_max(axis=0,color='grey'))''', language="python")
+#st.write(result_cols)
 
-#transforming into date time format and sorting
-df_api['time'] = pd.to_datetime(df_api['time'], format='%Y-%m-%d %H:%M:%S')
-df_api=df_api.sort_values(by=['time']).reset_index(drop=True)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Plotting graphs>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
 
-#splitting time components
-df_api['month'] = df_api.time.dt.month
-df_api['weekday']=df_api['time'].dt.weekday
-df_api=df_api.drop_duplicates(subset='id')
 
-#removing duplicates
-live_data=df.append(df_api)
-live_data=live_data.drop_duplicates(subset='id')
+#TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+st.write('-'*300)
+st.markdown("""<style>.big-temp {font-size:30px !important;text-align:center;text-decoration: underline;font-family: fantasy;background-color:#31333F;color:white}</style>""", unsafe_allow_html=True)
+st.markdown('<p class="big-temp">Temperature Distribution Plot</p>', unsafe_allow_html=True)
 
-#summary
-print("past data: {}".format(df.shape))
-print("api data: {}".format(df_api.shape))
-print("Full data: {}".format(live_data.shape))
+temp_dist=px.violin(dataframe,x='location',y='obj_temp')
+temp_dist.update_layout(showlegend=False,hovermode="x",template='simple_white')
+st.plotly_chart(temp_dist,sharing='streamlit',use_container_width=True)
 
-#saving data in streamlit folder
-live_data.to_csv(r'C:\Users\kvsekhar\veera\Neha work\all_data\temphum_all.csv',index=False,header=live_data.columns)
+#HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+st.write('-'*300)
+st.markdown("""<style>.big-hum {font-size:30px !important;text-align:center;text-decoration: underline;font-family: fantasy;background-color:#31333F;color:white}</style>""", unsafe_allow_html=True)
+st.markdown('<p class="big-hum">Humidity Distribution Plot</p>', unsafe_allow_html=True)
+
+hum_dist=px.violin(dataframe,x='location',y='humid')
+hum_dist.update_layout(showlegend=False,hovermode="x",template='simple_white')
+st.plotly_chart(hum_dist,sharing='streamlit',use_container_width=True)
+#11111111111111111111---------------------------------------OFFICE ROOM------------------------------------------------------------------1111111111111111111111
+st.write('-'*300)
+st.markdown("""<style>.big-ofc {font-size:30px !important;text-align:center;text-decoration: underline;font-family: fantasy;background-color:#31333F;color:white}</style>""", unsafe_allow_html=True)
+st.markdown('<p class="big-ofc">Office Room</p>', unsafe_allow_html=True)
+
+
+fig_ofc= make_subplots(specs=[[{"secondary_y": True}]])
+fig_ofc.add_trace(go.Scatter(x=ft_ofc.time, y=ft_ofc.obj_temp,
+                             mode='markers',name='Temperature',marker=dict(color='#FF1D00',size=2),
+         					 connectgaps=False),secondary_y=False)
+fig_ofc.add_trace(go.Scatter(x=ft_ofc.time, y=ft_ofc.humid,
+                             mode='markers',name='Humidity',marker=dict(color='#136EFF',size=2),
+                             connectgaps=False),secondary_y=True)
+fig_ofc.add_trace(go.Scatter(x=ft_ofc.time, y=ft_ofc.obj_temp,hoverinfo='skip',
+                             mode='lines',line=dict(color="#FF1D00",width=.5,dash='dash'),
+                             line_shape='spline',line_smoothing=1.3,connectgaps=True),secondary_y=False)
+fig_ofc.add_trace(go.Scatter(x=ft_ofc.time, y=ft_ofc.humid,hoverinfo='skip',
+                             mode='lines',line=dict(color="#136EFF",width=.5,dash='dash'),
+                             line_shape='spline',line_smoothing=1.3,connectgaps=True),secondary_y=True)
+
+
+fig_ofc.update_layout(showlegend=False,hovermode="x",title_font_family='Droid Serif',template='simple_white',title_text='<b>'+ft_ofc['location'].unique()[0]+'</b>',title_x=0.44)
+fig_ofc.update_xaxes(title_text="time")
+fig_ofc.update_yaxes(title_text="Temperature", secondary_y=False,range=[14,27])
+fig_ofc.update_yaxes(title_text="Humidity", secondary_y=True,range=[16,57])
+
+st.plotly_chart(fig_ofc, sharing='streamlit',use_container_width=True)
